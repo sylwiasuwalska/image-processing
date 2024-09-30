e91f52c89b76d9525b5af96e7bdd2826325fc1b3
sylwia.suwalska <sylwia.suwalska@olx.pl>
2024-09-30 19:06:57 +0200

    feature: add middleware resizeAndSaveImage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
 import path from 'path';
-import fs from 'fs';
-import sharp from 'sharp';
+
 import { checkQueryParams } from '../middleware/checkQueryParams';
-import { getQueryParams } from '../helpers/getQueryParams';
+
 import { resizeAndSaveImage } from '../middleware/resizeAndSaveImage';
 const routes = express.Router();
 
 const imagesDir = path.resolve(__dirname, '../../assets/full');
 
-routes.get('/', (req, res) => {
+routes.get('/', (_, res: Response) => {
   res.send(
     'Try /api/resize route with image name, width and height, example: /api/resize?image=fjord&width=300&height=500'
   );
